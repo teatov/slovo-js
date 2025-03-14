1081d0dc12e0599460df31b462e73b4f0f0f0abf
Teatov <hlebpoimenigleb@gmail.com>
2025-03-15 02:39:38 +1000

    add test for stem inferring

---
+++ test/slovo.test.ts
@@ -0,0 +1,15 @@
+import { test, expect } from 'vitest';
+import { Slovo } from '../src';
+
+test.each([
+  ['акула', 'акул'],
+  ['лыжня', 'лыжн'],
+  ['ведро', 'ведр'],
+  ['бельё', 'бель'],
+  ['край', 'кра'],
+  ['тополь', 'топол'],
+  ['поезд', 'поезд'],
+  ['стол', 'стол'],
+])('Slovo.stem(%s) -> %s', (actual, expected) => {
+  expect(Slovo.stem(actual)).toBe(expected);
+});
