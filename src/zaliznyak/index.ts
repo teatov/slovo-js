0ed5afd60d35e982e0c6f81ca7025c22eafa3c72
Teatov <hlebpoimenigleb@gmail.com>
2025-03-15 19:25:39 +1000

    move default `declension` value in constructor

--- src/zaliznyak/index.ts
+++ src/zaliznyak/index.ts
@@ -57,7 +57,7 @@ export class ZaliznyakIndex {
    * - `prn` - местоименное склонение
    * - `num` - склонение числительных
    */
-  declension?: typeof DECLENSION[number] = 'sub';
+  declension?: (typeof DECLENSION)[number];
 
   /**
    * Цифра индекса - тип склонения
@@ -128,7 +128,7 @@ export class ZaliznyakIndex {
 
   constructor({
     features,
-    declension,
+    declension = 'sub',
     type,
     mobileVowel,
     alternations,
