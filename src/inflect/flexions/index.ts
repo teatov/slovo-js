71a9fe83c3a58b63454796cb67b5fe49bd0630af
Teatov <hlebpoimenigleb@gmail.com>
2025-03-16 01:06:36 +1000

    add type 8 flexion

--- src/inflect/flexions/index.ts
+++ src/inflect/flexions/index.ts
@@ -15,9 +15,13 @@ export type GenderFlexion = (
  */
 const flexions: Record<ZaliznyakIndex['type'], Flexion> = {
   0: () => '',
+
   1: substantive1,
+
   2: substantive2,
+
   3: (index, inflection) => substantive1(index, inflection).replace('ы', 'и'),
+
   4: (index, inflection) => {
     if (
       (inflection.case === 'gen' ||
@@ -34,7 +38,9 @@ const flexions: Record<ZaliznyakIndex['type'], Flexion> = {
     }
     return substantive1(index, inflection).replace('ы', 'и').replace('о', 'е');
   },
+
   5: (index, inflection) => substantive1(index, inflection).replace('о', 'е'),
+
   6: (index, inflection) => {
     if (
       (inflection.case === 'gen' ||
@@ -51,6 +57,7 @@ const flexions: Record<ZaliznyakIndex['type'], Flexion> = {
     }
     return substantive2(index, inflection).replace('ь', 'й');
   },
+
   7: (index, inflection) => {
     if (
       (inflection.case === 'gen' ||
@@ -75,7 +82,38 @@ const flexions: Record<ZaliznyakIndex['type'], Flexion> = {
     }
     return substantive2(index, inflection).replace('ь', 'й');
   },
-  8: () => '',
+
+  8: (index, inflection) => {
+    switch (inflection.number) {
+      case 'sg':
+        switch (inflection.case) {
+          case 'nom':
+          case 'acc':
+            return 'ь';
+          case 'gen':
+          case 'dat':
+          case 'prp':
+            return 'и';
+          case 'ins':
+            switch (index.features.gender) {
+              case 'f':
+                return 'ью';
+              case 'm':
+              case 'n':
+                return 'ем';
+            }
+        }
+      case 'pl':
+        if (
+          inflection.case === 'gen' ||
+          (index.features.animacy === 'a' && inflection.case === 'acc')
+        ) {
+          return 'ей';
+        } else {
+          return substantive2(index, inflection);
+        }
+    }
+  },
 };
 
 export default flexions;
