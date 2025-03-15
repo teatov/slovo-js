c95d56f70154732d65f69210f7f7d2c928d2d1d3
Teatov <hlebpoimenigleb@gmail.com>
2025-03-15 15:18:42 +1000

    add test cases for `1a` and `2a` indexes

---
+++ test/flexionBasic.test.ts
@@ -0,0 +1,23 @@
+import { test, expect, describe } from 'vitest';
+import { ZaliznyakIndex } from '../src';
+import flexion from '../src/inflection/flexion';
+import type { Inflection } from '../src/inflection';
+import cases from './cases';
+
+describe.each(cases)('flexion[%s]', (indexString, cases) => {
+  const index = ZaliznyakIndex.fromString(indexString)!;
+  describe.each(cases)(
+    `flexion[${indexString}](%s) -> %s`,
+    (inflection, expected) => {
+      test('test', () => {
+        const [inflectionCase, number] = inflection.split('-');
+        expect(
+          flexion[index.type](index, {
+            case: inflectionCase as Inflection['case'],
+            number: number as Inflection['number'],
+          }),
+        ).toBe(expected);
+      });
+    },
+  );
+});
